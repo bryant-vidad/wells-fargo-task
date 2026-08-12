3df89ba51961eee0fc2b58c5b93aa4a1b272cb1f
Bryant Vidad <pinoi.son@gmail.com>
2026-08-12 14:15:39 -0400

    Add Security entity with properties and methods

---
+++ Security.java
@@ -0,0 +1,96 @@
+package entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Portfolio portfolio;
+
+    @Column
+    private String securityName;
+
+    @Column
+    private String category;
+
+    @Column
+    private LocalDate purchaseDate;
+
+    @Column
+    private BigDecimal purchasePrice;
+
+    @Column
+    private BigDecimal quantity;
+
+    protected Security() {
+    }
+
+    public Security(Portfolio portfolio, String securityName, String category,
+                     LocalDate purchaseDate, BigDecimal purchasePrice, BigDecimal quantity) {
+        this.portfolio = portfolio;
+        this.securityName = securityName;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getSecurityName() {
+        return securityName;
+    }
+
+    public void setSecurityName(String securityName) {
+        this.securityName = securityName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+}
